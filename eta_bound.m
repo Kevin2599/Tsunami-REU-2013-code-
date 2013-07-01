4179c064568dd0271290fd2aa54a0c36181f4165
Matthew <mh300@evansville.edu>
2013-07-01 12:10:30 -0800

    Have man branch

--- eta_bound.m
+++ eta_bound.m
@@ -1,8 +1,8 @@
 function y=eta_bound(x)
 	a=.0005;
-	x_l=8000;
+	x_l=9000;
 	l=2000;
-	y=a*exp(-((x-x_l)/l).^2);% gause wave
+	y=a*exp(-((x+x_l)/l).^2);% gause wave
     y(y<10^-5)=0;
 	%y(y~=0)=0;
    % y=.2/14.11;
